2d05dcb682234aedd4dccd3848ac70787211e613
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2023-09-04 18:59:55 -0500

    Add files via upload

---
+++ Modules.py
@@ -0,0 +1,14 @@
+import math
+import datetime
+import random
+a=5.6
+b=9
+print(math.ceil(a))
+print(math.floor(a))
+print(math.sqrt(b))
+print(math.sin(90))
+print(math.cos(0))
+print(datetime.datetime.now())
+print(datetime.datetime.today())
+a=random.randrange(1,10)
+print(a)
